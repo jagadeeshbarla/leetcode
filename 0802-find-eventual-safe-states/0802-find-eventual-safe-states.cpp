da0f7950ce3509cd069a4124093c09a8beadad05
Jagadeesh_Barla <93837542+jagadeeshbarla@users.noreply.github.com>
2023-07-12 16:58:45 +0530

    Time: 430 ms (5.05%), Space: 51.9 MB (54.95%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -5,30 +5,12 @@ public:
         vector<int>tns(graph.size(),0);
         vector<int>sns(graph.size(),0);
         vector<int>ans;
-        
-        
         for(int i=0;i<graph.size();i++)
         {
             if(graph[i].size()==0)
             {
                 tns[i]=1;
                 ans.emplace_back(i);
-            }
-        }
-        for(int i=0;i<graph.size();i++)
-        {
-            int flag = 0;
-            for(int j=0;j<graph[i].size();j++)
-            {
-                if(tns[graph[i][j]]==0)
-                {
-                    flag = 1;
-                }
-            }
-            if(flag == 0)
-            {
-                ans.emplace_back(i);
-                sns[i]=1;
             }
         }
         while(1)
