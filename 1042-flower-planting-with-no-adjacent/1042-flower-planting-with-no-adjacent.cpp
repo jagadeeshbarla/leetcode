c4ffb4fa23516afa549313c74a289a41cb95f961
Jagadeesh_Barla <93837542+jagadeeshbarla@users.noreply.github.com>
2024-02-27 23:02:20 +0530

    Time: 286 ms (8.25%), Space: 102.8 MB (5.13%) - LeetHub

---
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -0,0 +1,137 @@
+class Solution {
+public:
+    
+    int getnumber(vector<int>o)
+    {
+        int flag = 1;
+        for(int i = 0 ; i < o.size();i++)
+        {
+            if(o[i] == 1)
+            {
+                flag = 0;
+            }
+        }
+        if(flag == 1)
+        {
+            return 1;
+        }
+        
+        flag = 1;
+        for(int i = 0 ; i < o.size();i++)
+        {
+            if(o[i] == 2)
+            {
+                flag = 0;
+            }
+        }
+        if(flag == 1)
+        {
+            return 2;
+        }
+        
+        flag = 1;
+        for(int i = 0 ; i < o.size();i++)
+        {
+            if(o[i] == 3)
+            {
+                flag = 0;
+            }
+        }
+        if(flag == 1)
+        {
+            return 3;
+        }
+        
+        return 4;      
+    }
+    
+    
+    vector<int> gardenNoAdj(int n, vector<vector<int>>& path) {
+        
+        int s = path.size();
+        for(int i = 0 ; i < s ; i++)
+        {
+            vector<int>t;
+            t.emplace_back(path[i][1]);
+            t.emplace_back(path[i][0]);
+            path.emplace_back(t);
+        }
+        vector<int> vis(n,-1);
+        vector<int> ans(n,1);
+        map<int,vector<int>>m;
+        for(int i = 0 ; i < path.size() ;i++)
+        {
+            m[path[i][0]].emplace_back(path[i][1]);
+        }
+        map<int,vector<int>>occupied;
+        for(int i = 1 ; i <= n ; i++)
+        {
+            if(vis[i-1] == -1)
+            {
+                vis[i-1] = 1;
+                queue<int>q;
+                for(int j = 0 ; j < m[i].size() ; j++)
+                {
+                    occupied[m[i][j]].emplace_back(ans[i-1]);
+                    if(vis[m[i][j]-1] == -1)
+                    {
+                        q.push(m[i][j]);
+                        vis[m[i][j]-1] = 1;
+                    }
+                }
+                while(!q.empty())
+                {
+                    int x = q.front();
+                    ans[x-1] = getnumber(occupied[x]);
+                    
+                    for(int j = 0 ; j < m[x].size() ; j++)
+                    {
+                        occupied[m[x][j]].emplace_back(ans[x-1]);
+                        if(vis[m[x][j]-1] == -1)
+                        {
+                            q.push(m[x][j]);
+                            vis[m[x][j]-1] = 1;
+                        }
+                    }
+                    q.pop();                    
+                }
+            }
+        }
+        return ans;
+    }
+}; 
+
+
+
+// vector<int> vis(n,-1);
+//         vector<int> ans(n,1);
+        
+//         for(int i = 0 ; i < path.size() ;i++)
+//         {
+//             m[path[i][0]].emplace_back(path[i][1]);
+//         }
+        
+//         for(int i = 1 ; i <= n ; i++)
+//         {
+//             if(vis[i-1] == -1)
+//             {
+//                 vis[i-1] = 1;
+//                 queue<int>q;
+//                 q.push(i)
+//                 while(!q.empty())
+//                 {
+//                     int x = q.front();
+//                     q.pop();
+//                     for(int j = 0 ; j < m[x].size();j++)
+//                     {
+//                         if(vis[m[x][j]-1] == -1)
+//                         {
+//                             q.push(m[x][j]);
+//                             vis[m[x][j]-1] = 1;                        
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return ans;
