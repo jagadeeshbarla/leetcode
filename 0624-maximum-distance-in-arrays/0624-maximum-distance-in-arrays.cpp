695fc2edb759d6001fe11e7f68f32386d6920ece
Jagadeesh_Barla <93837542+jagadeeshbarla@users.noreply.github.com>
2024-08-17 11:34:54 +0530

    Time: 223 ms (77.83%), Space: 107.7 MB (76.89%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -1,21 +1,31 @@
 class Solution {
-    
 public:
-    
     int maxDistance(vector<vector<int>>& arrays) {
         
         int max_num = INT_MIN;
         int max_num2 = INT_MIN;
-        
         int min_num = INT_MAX;
         int min_num2 = INT_MAX;
         
+        int flag;
+        
         for(int i = 0 ; i < arrays.size() ; i++)
         {
+            //If first_maximum and first_minimum comes from one particular arrays[i]
+            if(max_num < arrays[i][arrays[i].size()-1] && min_num > arrays[i][0])
+            {
+                max_num2 = max_num;
+                max_num = arrays[i][arrays[i].size()-1];
+                min_num2 = min_num;
+                min_num = arrays[i][0];
+                flag = 0;
+                continue;
+            }
             if(max_num < arrays[i][arrays[i].size()-1])
             {
                 max_num2 = max_num;
                 max_num = arrays[i][arrays[i].size()-1];
+                flag = 1;
             }
             else if(max_num2 < arrays[i][arrays[i].size()-1])
             {
@@ -26,6 +36,7 @@ public:
             {
                 min_num2 = min_num;
                 min_num = arrays[i][0];
+                flag = 1;
             }
             
             else if(min_num2 > arrays[i][0])
@@ -35,17 +46,10 @@ public:
             
         }
         
-        for(int i = 0 ; i < arrays.size() ; i++)
+        if( flag == 0)
         {
-            if( max_num == arrays[i][arrays[i].size()-1] && min_num == arrays[i][0] ) // edge case
-            {
-                return max(    abs(max_num-min_num2) , abs(max_num2-min_num)    );
-                //cout<<max_num<<" "<<min_num2<<" "<<max_num2<<" "<<min_num<<endl;
-    
-            }
+            return max(    abs(max_num-min_num2) , abs(max_num2-min_num)    );
         }
-        
-        
         
         return abs(max_num - min_num);
     }
